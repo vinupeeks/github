6a3f9d10e4b3ebe72b1e012b4108e637fadfe5b3
vinupeeks <vinupeeks@gmail.com>
2025-02-10 16:06:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          // rel="noopener noreferrer"
+        // rel="noopener noreferrer"
         >
           Learn React
+          <h2>How to grow a stack over time</h2>
         </a>
       </header>
     </div>
