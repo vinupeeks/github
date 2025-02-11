1c56909a2cc8eb17630292f625c962a45abbca3f
vinupeeks <vinupeeks@gmail.com>
2025-02-11 17:28:19 +0530

    fd

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <App />
+    <App />
+    <App />
+    <App />
+    <App />
+    <App />
+    <App />
+    <App />
   </React.StrictMode>
 );
 
