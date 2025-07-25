112b629f36b87c545ab29c1e36c6665dd07c6569
vinupeeks <vinupeeks@gmail.com>
2025-07-25 20:30:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -25,9 +25,7 @@ function App() {
           <h2>How to grow a stack over time</h2>
           <h2>How to grow a stack over time</h2>
           <h2>How to grow a stack over time</h2>
-          <h2>How to grow a stack over time</h2> 
-          <h2>How to grow a stack over time</h2>
-          <h2>How to grow a stack over time</h2>
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2> 
           <h2>How to grow a stack over time</h2>
           <h2>How to grow a stack over time</h2>
