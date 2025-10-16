37edb6310d717766214942dcabe773c99bb6ca62
vinupeeks <vinupeeks@gmail.com>
2025-10-16 19:13:42 +0530

    exclouded tmsa 1A, 4A,.. like values in tmsa first graph

--- src/App.js
+++ src/App.js
@@ -574,7 +574,13 @@ function App() {
           <h2>How to grow a stack over time</h2>   
           <h2>How to grow a stack over time</h2>              
           <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>              
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>              
           <h2>How to grow a stack over time</h2>                
           <h2>How to grow a stack over time</h2>         
