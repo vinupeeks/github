59fcfd79cfc05b62ff9f9329c372f4049f6751c9
vinupeeks <vinupeeks@gmail.com>
2025-10-23 16:04:23 +0530

    multiple soc graph and export done

--- src/App.js
+++ src/App.js
@@ -531,7 +531,16 @@ function App() {
           <h2>How to grow a stack over time</h2>         
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>              
+          <h2>How to grow a stack over time</h2>            
+          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>     
+          <h2>How to grow a stack over time</h2>     
+          <h2>How to grow a stack over time</h2>              
           <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>              
+          <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>   
           <h2>How to grow a stack over time</h2>     
