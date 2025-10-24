b3dde04a07d80d6c4ed4c70c8d6770278609bd1f
vinupeeks <vinupeeks@gmail.com>
2025-10-24 15:25:50 +0530

    prediction ui updated with total viqs counts

--- src/App.js
+++ src/App.js
@@ -554,17 +554,7 @@ function App() {
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>              
+          <h2>How to grow a stack over time</h2>           
           <h2>How to grow a stack over time</h2>         
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>         
