0a4d99a6d5cc2bf452d504dc039ee046273cfb59
vinupeeks <vinupeeks@gmail.com>
2025-10-27 19:11:36 +0530

    predicgtion commented and soc graph added

--- src/App.js
+++ src/App.js
@@ -623,18 +623,7 @@ function App() {
           <h2>How to grow a stack over time</h2>              
           <h2>How to grow a stack over time</h2>         
           <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>         
           <h2>How to grow a stack over time</h2>   
