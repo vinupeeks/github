f3eac0d21b473d331b1b61f92908b7f8458c36a4
vinupeeks <vinupeeks@gmail.com>
2025-10-28 22:49:56 +0530

    top repeated viqs count updated

--- src/App.js
+++ src/App.js
@@ -276,17 +276,7 @@ function App() {
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>               
           <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>              
-          <h2>How to grow a stack over time</h2>            
-          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>                
