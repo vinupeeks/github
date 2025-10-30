535d513c0e513e19a0f6ec64ae74950221fdc7a0
vinupeeks <vinupeeks@gmail.com>
2025-10-30 19:31:49 +0530

    conflit resolved from chnages, excel export fltr

--- src/App.js
+++ src/App.js
@@ -275,6 +275,11 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>   
           <h2>How to grow a stack over time</h2>        
+          <h2>How to grow a stack over time</h2>        
+          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>
+          <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>   
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>   
