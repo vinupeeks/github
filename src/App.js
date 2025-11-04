eed695014d4c4f36bcb67a00b3725919f5cf8468
vinupeeks <vinupeeks@gmail.com>
2025-11-04 18:24:30 +0530

    dropdown filtered with unique items

--- src/App.js
+++ src/App.js
@@ -344,14 +344,7 @@ function App() {
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>
-          <h2>How to grow a stack over time</h2>      
-          <h2>How to grow a stack over time</h2>
-          <h2>How to grow a stack over time</h2>
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>      
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>   
