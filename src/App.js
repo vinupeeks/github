0daf5fd6b91f416ea031de39615ce124a46fb63a
vinupeeks <vinupeeks@gmail.com>
2025-11-07 19:05:24 +0530

    prediction export updated

--- src/App.js
+++ src/App.js
@@ -377,12 +377,7 @@ function App() {
           <h2>How to grow a stack over time</h2>       
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>   
           <h2>How to grow a stack over time</h2>        
         </a>
