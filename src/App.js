adb5f45b620a53451ed55aad248d26a7ab1126e7
vinupeeks <vinupeeks@gmail.com>
2025-11-13 11:29:15 +0530

    viqs wise sorting complted in prediction export

--- src/App.js
+++ src/App.js
@@ -591,14 +591,6 @@ function App() {
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>    
         </a>
       </header>
