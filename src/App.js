9e96b81ddcb5aa8ba1cce1c7dfeaabacc2f11fa1
vinupeeks <vinupeeks@gmail.com>
2025-11-14 22:52:00 +0530

    tmsa full graphs displayy issue fixed

--- src/App.js
+++ src/App.js
@@ -600,9 +600,7 @@ function App() {
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>   
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>    
