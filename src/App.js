7615ff7ca2b68ae8d568f2c827be27d38c7ea8eb
vinupeeks <vinupeeks@gmail.com>
2025-11-18 09:40:24 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -472,10 +472,7 @@ function App() {
           <h2>How to grow a stack over time</h2>       
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>    
