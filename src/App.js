59a6ca813d4615805a6d1227ad6c6ff6549a042d
vinupeeks <vinupeeks@gmail.com>
2025-11-20 19:25:26 +0530

    Fix: ignore Android build files

--- src/App.js
+++ src/App.js
@@ -352,12 +352,7 @@ function App() {
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>     
+          <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>    
         </a>
