df88308c1843f5f285d1b079be49b0c5cca3ca48
vinupeeks <vinupeeks@gmail.com>
2025-11-24 22:53:28 +0530

    updated some questions module

--- src/App.js
+++ src/App.js
@@ -295,11 +295,7 @@ function App() {
           <h2>How to grow a stack over time</h2>   
           <h2>How to grow a stack over time</h2> 
           <h2>How to grow a stack over time</h2>      
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2> 
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>      
-          <h2>How to grow a stack over time</h2>        
+          <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>        
