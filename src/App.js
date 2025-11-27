200eb2c58919cc53a2a80c8342cc7db03af057fb
vinupeeks <vinupeeks@gmail.com>
2025-11-27 20:29:29 +0530

    first cut done

--- src/App.js
+++ src/App.js
@@ -362,10 +362,6 @@ function App() {
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>      
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>      
-          <h2>How to grow a stack over time</h2>      
-          <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>   
           <h2>How to grow a stack over time</h2> 
           <h2>How to grow a stack over time</h2> 
