b2dcfc72cd03d12fcdd0ebce1b4f4abcbe987ba4
vinupeeks <vinupeeks@gmail.com>
2025-12-01 18:53:40 +0530

    hotel unpushed codes added

--- src/App.js
+++ src/App.js
@@ -397,10 +397,6 @@ function App() {
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>      
