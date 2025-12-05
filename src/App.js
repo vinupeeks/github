a0f96a82f3a84b502765235a5d13d4c99dd7a797
vinupeeks <vinupeeks@gmail.com>
2025-12-05 23:47:48 +0530

    commit before the DB sync

--- src/App.js
+++ src/App.js
@@ -593,16 +593,7 @@ function App() {
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>         
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>       
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>        
