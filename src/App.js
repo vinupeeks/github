78bfb6d2dca84908e79f544d99ccd99e0a5ecb39
vinupeeks <vinupeeks@gmail.com>
2025-12-11 23:18:40 +0530

    tmsa half yearly split done

--- src/App.js
+++ src/App.js
@@ -395,16 +395,7 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>                
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>        
+          <h2>How to grow a stack over time</h2>       
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>  
