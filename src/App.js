d9086c7a446871313cc990c936f71b0813e6edae
vinupeeks <vinupeeks@gmail.com>
2025-12-16 20:01:07 +0530

    colour indicatror splited 0-30

--- src/App.js
+++ src/App.js
@@ -506,6 +506,14 @@ function App() {
           <h2>How to grow a stack over time</h2>       
           <h2>How to grow a stack over time</h2>                
           <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>       
+          <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>       
+          <h2>How to grow a stack over time</h2>                  
+          <h2>How to grow a stack over time</h2>        
+          <h2>How to grow a stack over time</h2>       
+          <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>       
