3f46424fb4cd2710a118d1945706b0a083240858
vinupeeks <vinupeeks@gmail.com>
2025-12-17 19:07:15 +0530

    done with some admin data listing

--- src/App.js
+++ src/App.js
@@ -524,16 +524,6 @@ function App() {
           <h2>How to grow a stack over time</h2>        
           <h2>How to grow a stack over time</h2>       
           <h2>How to grow a stack over time</h2>       
-          <h2>How to grow a stack over time</h2>                
-          <h2>How to grow a stack over time</h2>       
-          <h2>How to grow a stack over time</h2>                  
-          <h2>How to grow a stack over time</h2>        
-          <h2>How to grow a stack over time</h2>       
-          <h2>How to grow a stack over time</h2>                
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>                
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>       
           <h2>How to grow a stack over time</h2>                  
           <h2>How to grow a stack over time</h2>        
