bba722d70ad6400825015e9a0b0b2bf3aacf96ba
vinupeeks <vinupeeks@gmail.com>
2025-12-22 09:37:20 +0530

    some changes aaded, tmsa issue, and lock and unlock

--- src/App.js
+++ src/App.js
@@ -639,14 +639,7 @@ function App() {
           <h2>How to grow a stack over time</h2>                
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>                
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>            
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>      
+          <h2>How to grow a stack over time</h2> 
           <h2>How to grow a stack over time</h2>    
         </a>
       </header>
