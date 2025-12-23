c24d7946ab7069cd23097be557e0362c336297b7
vinupeeks <vinupeeks@gmail.com>
2025-12-23 21:24:40 +0530

    quiz syncing done

--- src/App.js
+++ src/App.js
@@ -653,16 +653,7 @@ function App() {
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>      
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>                
-          <h2>How to grow a stack over time</h2>          
-          <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>            
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>           
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2> 
           <h2>How to grow a stack over time</h2>    
