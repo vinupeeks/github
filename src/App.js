bf5934fdefe56d6e3b838b0c6e45d5cc677f192d
vinupeeks <vinupeeks@gmail.com>
2025-12-29 19:14:14 +0530

    issues fixed and certificate preview added

--- src/App.js
+++ src/App.js
@@ -732,6 +732,18 @@ function App() {
           <h2>How to grow a stack over time</h2>                  
           <h2>How to grow a stack over time</h2>       
           <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>                               
+          <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>               
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>                  
+          <h2>How to grow a stack over time</h2>       
+          <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
