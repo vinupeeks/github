ac53f796efdc913963b9e944d10f76087089ffa5
vinupeeks <vinupeeks@gmail.com>
2025-12-30 23:14:51 +0530

    sync api updated(priority training created when sync)

--- src/App.js
+++ src/App.js
@@ -338,7 +338,17 @@ function App() {
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>      
+          <h2>How to grow a stack over time</h2>                  
           <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>      
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>                
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>          
+          <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>                
           <h2>How to grow a stack over time</h2>          
           <h2>How to grow a stack over time</h2>          
