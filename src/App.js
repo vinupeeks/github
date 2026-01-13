e849089f2304bdcbc9f48b30756a61ed7153d197
vinupeeks <vinupeeks@gmail.com>
2026-01-14 00:20:55 +0530

    stylianos points updated, piechart, tmsa bar values and ordering, export scaleup

--- src/App.js
+++ src/App.js
@@ -344,6 +344,16 @@ function App() {
           <h2>How to grow a stack over time</h2>         
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>         
           <h2>How to grow a stack over time</h2>   
