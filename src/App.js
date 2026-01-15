1b1e55e3d1a8a5b042d823e1bba33d0f32299b4b
vinupeeks <vinupeeks@gmail.com>
2026-01-15 21:37:38 +0530

    charts broken issue fixed

--- src/App.js
+++ src/App.js
@@ -283,6 +283,13 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>    
+          <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>         
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>         
