842d9422e44be9a2383b081c8949f344c99ce330
vinupeeks <vinupeeks@gmail.com>
2026-01-23 19:28:49 +0530

    destinations display after the category and type selection

--- src/App.js
+++ src/App.js
@@ -344,6 +344,14 @@ function App() {
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>    
+          <h2>How to grow a stack over time</h2>    
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
