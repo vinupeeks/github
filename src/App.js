9b00707e51d1a57ca6d03b6bcfbffbf8085e69f7
vinupeeks <vinupeeks@gmail.com>
2026-01-28 19:19:51 +0530

    merge done and updated on dev

--- src/App.js
+++ src/App.js
@@ -387,15 +387,6 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>    
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
