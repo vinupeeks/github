b91d99800743fb01768503ef85f27afd61dddc30
vinupeeks <vinupeeks@gmail.com>
2026-01-28 19:19:37 +0530

    merge done and updated on dev

--- src/App.js
+++ src/App.js
@@ -362,54 +362,7 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
+
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
