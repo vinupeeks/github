d4decd14d4511fbfcbad06793ddea71b21db19c7
vinupeeks <vinupeeks@gmail.com>
2026-02-02 23:26:24 +0530

    updated pdf export and Rich text data

--- src/App.js
+++ src/App.js
@@ -348,15 +348,6 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
