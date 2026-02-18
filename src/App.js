4715e4400e9125cfa917735e762e21d145ead653
vinupeeks <vinupeeks@gmail.com>
2026-02-18 20:25:29 +0530

    course and college reasons combined

--- src/App.js
+++ src/App.js
@@ -384,20 +384,7 @@ function App() {
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>    
+          <h2>How to grow a stack over time</h2>      
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
