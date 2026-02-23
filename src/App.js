b6c758a05dee11ca4e7d52ecfacb37c7db356580
vinupeeks <vinupeeks@gmail.com>
2026-02-23 17:44:12 +0530

    drawer changes reverted

--- src/App.js
+++ src/App.js
@@ -286,6 +286,18 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>    
+          <h2>How to grow a stack over time</h2>     
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>     
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>   
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
