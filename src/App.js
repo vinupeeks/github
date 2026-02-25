9b79882521188483a8d0dbcb2ab1f9df20b8fe39
vinupeeks <vinupeeks@gmail.com>
2026-02-25 19:15:28 +0530

    manual scored items have or not flag added

--- src/App.js
+++ src/App.js
@@ -365,24 +365,6 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2> 
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2> 
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>     
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2> 
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>   
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>     
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2> 
