fb7c1b427d56e4f5af8011bdb9ace76c48de40b4
vinupeeks <vinupeeks@gmail.com>
2026-02-27 19:37:42 +0530

    cargo type editable now only super and company admins

--- src/App.js
+++ src/App.js
@@ -416,22 +416,7 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>    
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
-          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2> 
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
