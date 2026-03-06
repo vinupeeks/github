444b5514496942b4c8d9b5d3ab84effa14773e89
vinupeeks <vinupeeks@gmail.com>
2026-03-06 20:07:01 +0530

    updated some feedback points, and ui in progressing.

--- src/App.js
+++ src/App.js
@@ -333,6 +333,21 @@ function App() {
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>    
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2> 
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
+          <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
           <h2>How to grow a stack over time</h2>  
